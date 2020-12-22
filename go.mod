0caad30cfb803b89eb5c1d4c9aea4d0e2da00ed1
TranDucDung <cbcbvn99@gmail.com>
2020-12-22 15:56:27 +0700

    add

--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module orderfood
 go 1.15
 
 require (
+	github.com/SermoDigital/jose v0.9.1
 	github.com/astaxie/beego v1.12.3
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gbrlsnchs/jwt v1.1.0
+	github.com/gbrlsnchs/jwt/v3 v3.0.0
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/magefile/mage v1.10.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- routers/router.go
+++ routers/router.go
@@ -4,13 +4,22 @@ import (
 	"orderfood/controllers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func init() {
-	ns := beego.NewNamespace("/v1/orderfood",
+	beego.Get("/", func(ctx *context.Context) {
+		_ = ctx.Output.Body([]byte("This is a Beego + JWT API - Creator: Mehran Abghari (mehran.ab80@gmail.com)"))
+	})
+	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/statisticstore",
 			beego.NSInclude(
 				&controllers.StatisticStoreController{},
+			),
+		),
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
 			),
 		),
 	)
